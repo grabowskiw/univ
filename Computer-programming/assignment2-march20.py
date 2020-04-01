537b8b4ccf7c0957556d1c60ac3a637a4774cb4c
Wojtek <42513295+grabowskiw@users.noreply.github.com>
2020-04-01 20:58:18 +0200

    Assignment 2, March 20

---
+++ Computer-programming/assignment2-march20.py
@@ -0,0 +1,37 @@
+"""
+Python 2.7.17 (v2.7.17:c2f86d86e6, Oct 19 2019, 21:01:17) [MSC v.1500 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+"""
+print "Hello Tomek!"
+# Hello Tomek!
+n = ""
+b = bool(1)
+s = str("Text")
+i = int(3)
+f = float(4.25)
+l = [1, 2, 3]
+print l
+# [1, 2, 3]
+l = (1, 2, 3)
+print l
+# (1, 2, 3)
+l = {"a": 1, "b": 2}
+print l
+# {'a': 1, 'b': 2}
+print l["a"]
+# 1
+a, b, c, d = 10, 11, 15, 17
+print b
+# 11
+print c
+# 15
+sum = i + f
+print sum
+# 7.25
+type(n) # <type 'str'>
+type(b) # <type 'int'>
+type(s) # <type 'str'>
+type(i) # <type 'int'>
+type(f) # <type 'float'>
+type(l) # <type 'dict'>
+        # wy¿ej jest dict, bo jako ostatnie przypisa³em ma³e L jako s³ownik
